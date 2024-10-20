c0d2eb9d60c9a6c7f67f5075451c678594dd9bd7
Rajesh97266 <rajesh97266@gmail.com>
2024-10-20 14:36:41 +0530

    second-commit

--- script.js
+++ script.js
@@ -2,12 +2,37 @@
 
 // document.querySelector('.message').textContent = 'Correct Answer 😁😁😁';
 // console.log(document.querySelector('.guess').value);
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Content';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉🎉 Correct Number!!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game';
+
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game';
+
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
